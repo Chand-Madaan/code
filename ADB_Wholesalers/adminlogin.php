fde73c7e5cb4be6046180b549c130409154e049a
Inderpreet0215 <64117011+Inderpreet0215@users.noreply.github.com>
2020-04-29 23:35:22 +0930

    Add files via upload

--- ADB_Wholesalers/adminlogin.php
+++ ADB_Wholesalers/adminlogin.php
@@ -1,21 +1,41 @@
 <!DOCTYPE>
 <html>
 <head>
-    <title> LOGIN</title>
+    <meta charset="UTF-8">
+    <title> LOGIN </title>
+    <link rel="stylesheet" href="css/style1.css">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
 <div class="container">
+    <nav class="navbar">
+        <a href="dashboard.php">
+            <h1>ADB WHOLESALERS</h1></a>
+    </nav>
+
     <form action="adminloginprocess.php" method="post">
-        <div class="form-group">
-            <input class="form-control" placeholder="username" name="admin_user" type="text">
-        </div>
-        <div class="form-group">
-            <input class="form-control" placeholder="password" name="admin_password" type="password">
-        </div>
+            <div class="f">
+                <h1>Welcome To Login</h1>
+                <div class="login">
+                    <input class="form-control1" placeholder="username" name="admin_user" type="text">
+            <input class="form-control2" placeholder="password" name="admin_password" type="password">
+                </div>
+                <div class="button">
+
         <button class="btn btn-primary" type="submit" name="login">Sign In </button>
+         </div>
+            </div>
     </form>
-</div>
+
 </body>
 </html>
+            <!-- <input class="form-control" placeholder="username" name="admin_user" type="text">
+            <input class="form-control2" placeholder="password" name="admin_password" type="password">
+        <!-- </div>
+        <div class="form-group">
+        <input class="form-control2" placeholder="password" name="admin_password" type="password">
+        </div>
+
+         <button class="btn btn-primary" type="submit" name="login">Sign In </button>
+    </body
